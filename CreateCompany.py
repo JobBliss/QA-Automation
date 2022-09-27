5eb24e1f14c097d4f4e5686762617e5393ccaad9
Anesu WekwaChiodza <112934872+ndiniBlackShifu@users.noreply.github.com>
2022-09-27 12:39:18 +0200

    Update CreateCompany
    Dynamic and quick execution

--- CreateCompany.py
+++ CreateCompany.py
@@ -3,6 +3,8 @@ import time
 import selenium
 import logging
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
@@ -16,32 +18,34 @@ from Screenshot import *
 
 def jobblissCreateCompany():
 
-    logging.basicConfig(filename='log.txt', level=logging.DEBUG, format="%(asctime)s %(message)s", filemode='w')
+    logging.basicConfig(filename='Createcompany_log.txt', level=logging.DEBUG, format="%(asctime)s %(message)s", filemode='w')
     # Complete these fields ==================
-    USERNAME = 'anesuchiodza@yahoo.com'
+    USERNAME = 'anesuchiodza+ce@yahoo.com'
     PASSWORD = 'testCase123_'
-    FNAME = 'Moses'
-    LNAME= 'Gumbochuma'
-    NOEMPLOYEES='4'
+    FNAME = 'Rodney'
+    LNAME= 'Cleanshizoo'
+    NOEMPLOYEES='3'
     CITY = 'Honolulu'
-    BNAME = 'GreenBottle Investments'
+    BNAME = 'Juice Bottle Inc'
     # ==========================================
     fscreenshot = Screenshot.Screenshot()
 
-
-    TIMEOUT = 10
+    TIMEOUT = 5
 
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    #options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument("--log-level=3")
+    #options.add_argument("--incognito")
+    options.add_argument("--kiosk")
 
     browser = webdriver.Chrome(executable_path=CM().install(), options=options)
+    
     #LoginToCompany
+        
     browser.get(
         'https://alpha.jobbliss.com/signup')
 
-    time.sleep(2)
 
     print("[Info] - Logging in...")
 
@@ -49,102 +53,106 @@ def jobblissCreateCompany():
     #clickCompany Button
     clickCompany = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div/div/div[1]/div/div/button')))
+            By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div[2]/div/div/div/div/div[1]/div/div/button')))
 
     clickCompany.click()
-    time.sleep(3)
-    browser.maximize_window()
+        
 
     #Enter Company Email Address
     user_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/span[1]/div/div[2]/div[1]/div/input')))
+            By.TAG_NAME , "input")))
 
     user_element.send_keys(USERNAME)
 
     # Enter First Name
     firstname_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/span[2]/div/div[2]/div/div/input')))
+            By.NAME,
+            'givenName')))
 
     firstname_element.send_keys(FNAME)
-    time.sleep(1)
+    
 
     # Enter Last Name
     lastname_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/span[3]/div/div[2]/div[1]/div/input')))
+            By.NAME,
+            'familyName')))
 
     lastname_element.send_keys(LNAME)
-    time.sleep(1)
+    
 
     # PassWord
     pass_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/span[4]/div/div[2]/div[1]/div/input')))
+            By.NAME, 'password')))
 
     pass_element.send_keys(PASSWORD)
-    time.sleep(1)
+    
 
     # PassWord
     confirm_pass_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/span[5]/div/div[2]/div[1]/div/input')))
+            By.NAME,
+            'confirm')))
 
     confirm_pass_element.send_keys(PASSWORD)
-    time.sleep(1)
+    
     #------------------
     #Business Info:
     #-----------------
     #Business Name
     businessname_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/div/span[1]/div/div[2]/div[1]/div/input')))
+            By.NAME,
+            'business_name')))
 
     businessname_element.send_keys(BNAME)
-    time.sleep(1)
+    
 
     #City
     city_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/div/span[2]/div/div[2]/div[1]/div/input')))
+            By.NAME,
+            'business_city')))
 
     city_element.send_keys(CITY)
-    time.sleep(1)
+    
 
     #Number of Employees
 
     no_employees_element = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH,
-            '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[1]/div/div/div/span[3]/div/div[2]/div/div/input')))
+            By.NAME,
+            'Employees')))
 
     no_employees_element.send_keys(NOEMPLOYEES)
 
-    time.sleep(10)
+   
 
     signup_button = WebDriverWait(browser, TIMEOUT).until(
         EC.presence_of_element_located((
-            By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[1]/div[2]/form/div[2]/div[2]/div/button')))
+            By.ID, 'signup'
+            )))
     print("signing up now")
 
     signup_button.click()
 
-    time.sleep(3)
+    time.sleep(5)
 
     # Take a screenshot of the Page and store it for analysis.
-    image = fscreenshot.full_Screenshot(browser, save_path=r'.', image_name='CreateAccCompany.png')
+    image = fscreenshot.full_Screenshot(browser, save_path=r'.', image_name='CreateCompanyAcc.png')
     time.sleep(3)
-    screenshot = Image.open('CreateAccCompany.png')
-    screenshot.show()
+    print("You have successfully logged in.")
+    screenshot = Image.open('CreateCompanyAcc.png')
+    #screenshot.show()
 
+    time.sleep(3)
+    print("Closing browser now...")
+    browser.quit()
 
+    
 
 if __name__ == '__main__':
     jobblissCreateCompany()
